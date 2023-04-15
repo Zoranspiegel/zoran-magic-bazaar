dcbe04e4dd256a11feef4e44609f51d79817f4ef
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-15 13:27:10 -0500

    config/mongoose DB connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
+} = process.env;
 const server = require('./src/app');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3002;
 
-server.listen(PORT, () => {
-  console.log(`🧞Listening on port ${PORT}...`);
-});
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@a30l14n.uk1f40m.mongodb.net/${DB_NAME}?retryWrites=true`)
+  .then(() => {
+    console.log(`🍃Connected to ${DB_NAME} MongoDB`);
+    server.listen(PORT, () => {
+      console.log(`🧞Listening on port ${PORT}...`);
+    });
+  });
