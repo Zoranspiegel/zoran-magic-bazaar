d8e90687d3ccff0552df0bac5dea5a3f16b076ea
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-15 15:58:23 -0500

    Railway api deploy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const {
-  DB_USER,
-  DB_PASSWORD,
+  DB_DEPLOY,
   DB_NAME
 } = process.env;
 const server = require('./src/app');
@@ -9,7 +8,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3002;
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@a30l14n.uk1f40m.mongodb.net/${DB_NAME}?retryWrites=true`)
+mongoose.connect(DB_DEPLOY)
   .then(() => {
     console.log(`🍃Connected to ${DB_NAME} MongoDB`);
     server.listen(PORT, () => {
