def2eb9aea506cf292727a2a2ed220c3fa9fbce7
Juan José Rodriguez <zoranbow@gmail.com>
2023-04-16 00:36:43 -0500

    add/components/Loading & Error

--- client/src/components/Deck.jsx
+++ client/src/components/Deck.jsx
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import Card from './Card';
+import Loading from './Loading';
+import Error from './Error';
 
 const Deck = () => {
   const products = useSelector(state => state.products);
+  const status = useSelector(state => state.products_status);
 
   return (
     <StyledDeck>
+      {status.loading === 'pending' && <Loading />}
+      {status.loading === 'rejected' && <Error error={status.error} />}
       {products?.map(p => <Card key={p._id} product={p} />)}
     </StyledDeck>
   );
---
+++ client/src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({ error }) => {
+  return (
+    <div>
+      <h1>{error}</h1>
+    </div>
+  );
+};
+
+export default Error;
